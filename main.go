d32c13630951c343d302c0ca83c494d378e231e9
prodonik <rruzibayevich@gmail.com>
2024-04-15 18:06:01 +0500

    hmw14

---
+++ main.go
@@ -0,0 +1,15 @@
+// 1) 12 chi uy vazifada ValidateUser funksiya yaratilgan edi.
+// 2) Bugungi test_repo repository yaratilgan ichida o'zgarishlar kiritilishi kerak
+// 3) ValidateUser dasturri test_repo ni ichiga joylang
+// 4) user package yaratib uning ichida Validate digan funksiyani e'lon qiling
+// 6) main package da user.Validate qilingan holda chaqirinsin
+// 7) dastur ishlashini hosil qiling (go run yoki go build o'rqali)
+// 8) barchasi ishlagan taqdirda, hamma o'zgarishlarni git add, git commit va git push commandar o'rqali bajaring
+// 9) natijada local codelaringiz github ga joylangan bolishi kerak
+// 10) Repository linkini jonating
+
+package main
+
+func main() {
+
+}
